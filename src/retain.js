b992a73ad682e75c03fa5e5ff702ff0b69da8c8a
duanhaochen <duanhaochen@126.com>
2018-02-08 11:49:53 +0800

    添加了新的枪毙类型

--- src/retain.js
+++ src/retain.js
@@ -3,7 +3,7 @@
 // 文件
 const file: string[] = ['license', '.npmignore', 'package-lock.json', 'yarn.lock', '.yarn-integrity', '.gitignore'];
 // 扩展名
-const ext: string[] = ['.md', '.txt'];
+const ext: string[] = ['.md', '.txt', '.markdown'];
 
 module.exports = {
   file,
